2fbf95a78db65ed0741496d1800a0686c87cee17
mdeibrahim <mmeibrahim505@gmail.com>
2025-12-09 03:08:01 +0600

    deploy seccessful

--- project/settings.py
+++ project/settings.py
@@ -192,6 +192,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
